ed9f4bef5567e6526c65b1f36da5c810a8c4bdac
julianoviedo <julianoviedo1@gmail.com>
2020-07-22 16:35:44 -0300

    hecho con bootstrap, queda elavorar mas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = %w[chinese italian japanese french belgian]
+puts 'starting seed'
+10.times do
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: category.sample)
+  restaurant.save!
+end
+puts 'finishing seed'
